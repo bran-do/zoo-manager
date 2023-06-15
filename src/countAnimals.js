aa27edc3343ed6d3f38b5ef5f933a901e6fc493a
Felipe Brandão <brandaofelipebrandao@gmail.com>
2023-06-15 18:48:09 -0300

    6. Obtenha ao menos 80% de cobertura de testes na função handlerElephants && 7. Obtenha ao menos 90% de cobertura de testes na função handlerElephants

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -11,8 +11,6 @@ const allAnimalsCount = () => {
   return finalCount;
 };
 
-console.log(allAnimalsCount());
-
 const countAnimals = (animal) => {
   if (animal === undefined) {
     return allAnimalsCount();
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,32 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica o parâmetro "count" da função', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('Verifica o parâmetro "names" da função', () => {
+    expect(handlerElephants('names')).toStrictEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(handlerElephants('names')).toContain('Ilana');
+    expect(handlerElephants('names')).toContain('Orval');
+    expect(handlerElephants('names')).toContain('Bea');
+    expect(handlerElephants('names')).toContain('Jefferson');
+  });
+  it('Verifica o parâmetro "averageAge" da função', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+    expect(handlerElephants('averageAge')).toEqual((11 + 15 + 12 + 4) / 4);
+  });
+  it('Verifica o parâmetro "location" da função', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('Verifica o parâmetro "popularity" da função', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+  it('Verifica o parâmetro "availability" da função', () => {
+    expect(handlerElephants('availability')).toStrictEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+  it('Verifica parâmetros inválidos da função', () => {
+    expect(handlerElephants('age')).toBeNull();
+    expect(handlerElephants(15)).toBe('Parâmetro inválido, é necessário uma string');
+    expect(handlerElephants()).toBeUndefined();
+  });
+});
