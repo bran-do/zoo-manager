80901a39cb1a64a6e080cd60fdab2e3ca8dade88
felipe <brandaofelipebrandao@gmail.com>
2024-05-23 20:07:40 -0300

    Adiciona descrição de getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,3 +1,8 @@
+/*
+  A função `getSpeciesByids` retorna todas as espécies do `zoo_data` que correspondem aos `id`s passados como parâmetro.
+  O parâmetro ser tanto um único `id` como um array de `id`.
+*/
+
 const data = require('../data/zoo_data');
 
 const { species } = data;
