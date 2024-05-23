01c8d6c34193e3d95540fb9fd29568a2b81fe192
felipe <brandaofelipebrandao@gmail.com>
2024-05-23 20:13:11 -0300

    Refatoração final getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,14 +5,12 @@
 
 const data = require('../data/zoo_data');
 
-const { species } = data;
-
 const getSpeciesByIds = (...ids) => {
   if (ids.length === 1) {
-    return species.filter((animal) => animal.id === ids[0]);
+    return data.species.filter((species) => species.id === ids[0]);
   }
 
-  return species.filter((animal, index) => animal.id === ids[index]);
+  return data.species.filter((species, index) => species.id === ids[index]);
 };
 
 module.exports = getSpeciesByIds;
