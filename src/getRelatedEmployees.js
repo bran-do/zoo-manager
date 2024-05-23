432606bdc2d7721a88cfc9d5258eb91c0ef784f8
felipe <brandaofelipebrandao@gmail.com>
2024-05-23 20:38:55 -0300

    Refatoração e descrição getRelatedEmployees

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,14 +1,21 @@
+/*
+  A função `getRelatedEmployees` retorna o nome completo de todos os funcionários subordinados ao gerente de `id` passado como parâmetro.
+  Caso a pessoa com esse `id` não seja gerente, a função lança um erro informando tal -- essa verificação é feita pela função `isManager`.
+*/
+
 const data = require('../data/zoo_data');
 
-const isManager = (id) => data.employees.some((employee) => employee.managers.includes(id));
+const { employees } = data;
+
+const isManager = (id) => employees.some((employee) => employee.managers.includes(id));
 
 const getRelatedEmployees = (managerId) => {
-  if (isManager(managerId) === false) {
+  if (!isManager(managerId)) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
 
-  const employeesObjs = data.employees.filter((employee) => employee.managers.includes(managerId));
-  return employeesObjs.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  const subordinates = employees.filter((employee) => employee.managers.includes(managerId));
+  return subordinates.map((subordinate) => `${subordinate.firstName} ${subordinate.lastName}`);
 };
 
 module.exports = { isManager, getRelatedEmployees };
