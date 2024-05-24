2a37042b1c218c66c5ca5532704776d77bd48487
felipe <brandaofelipebrandao@gmail.com>
2024-05-24 01:53:23 -0300

    Refatoração da função getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,14 +1,57 @@
 const data = require('../data/zoo_data');
 
-// const species = data.species;
+const { species, hours } = data;
+const weekdays = Object.keys(hours);
 
-// const allSpeciesNames = species.map((species) => species.name);
+const getSpeciesAvailability = (speciesName) => {
+  const { availability } = species.find((dataSpecies) => dataSpecies.name === speciesName);
+  return availability;
+};
+
+const getOfficeHour = (weekday) => {
+  const { open, close } = hours[weekday];
+  return open
+    ? `Open from ${open}am until ${close}pm`
+    : 'CLOSED';
+};
+
+const getExhibition = (weekday) => {
+  const exhibition = species.filter(({ availability }) => availability.includes(weekday));
+  return exhibition.length > 0
+    ? exhibition.map(({ name }) => name)
+    : 'The zoo will be closed!';
+};
+
+const getDailySchedule = (weekday) => {
+  const officeHour = getOfficeHour(weekday);
+  const exhibition = getExhibition(weekday);
+  return { [`${weekday}`]: { officeHour, exhibition } };
+};
+
+const getWeeklySchedule = () => {
+  let weeklySchedule = {};
+
+  for (let i = 0; i < weekdays.length; i += 1) {
+    const weekday = weekdays[i];
+    const dailySchedule = getDailySchedule(weekday);
+    weeklySchedule = { ...weeklySchedule, ...dailySchedule };
+  }
+
+  return weeklySchedule;
+};
 
 const getSchedule = (scheduleTarget) => {
-  // if (allSpeciesNames.includes(scheduleTarget)) {
-  //   return species[allSpeciesNames.indexOf(scheduleTarget)].availability;
-  // }
+  const speciesNames = species.map(({ name }) => name);
 
+  if (speciesNames.includes(scheduleTarget)) {
+    return getSpeciesAvailability(scheduleTarget);
+  }
+
+  if (weekdays.includes(scheduleTarget)) {
+    return getDailySchedule(scheduleTarget);
+  }
+
+  return getWeeklySchedule();
 };
 
 module.exports = getSchedule;
