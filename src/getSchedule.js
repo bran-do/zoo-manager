2eb156ee795ffa24b8a0bcefee401116fb1680b1
felipe <brandaofelipebrandao@gmail.com>
2024-05-24 02:11:08 -0300

    Adiciona descrição da função getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,13 +1,24 @@
+/*
+  A função `getSchedule` é responsável por retornar o cronograma de visita baseado em uma espécie do zoológico ou em um dia da semana;
+    - Se o parâmetro for uma espécie (ex: 'lions'), a função retorna os dias da semana em que essa espécie está disponível para exibição.
+    - Se o parâmetro for um dia da semana (ex: 'Friday'), a função retorna as espécies disponíveis para exibição e o horário de funcionamento do zoológico nesse dia.
+    - Se a função for chamada sem parâmetros, ela retorna todos os dias da semana e suas respectivas exibições e horários de funcionamento.
+
+  As explicações das funções auxiliares estão abaixo.
+*/
+
 const data = require('../data/zoo_data');
 
 const { species, hours } = data;
 const weekdays = Object.keys(hours);
 
+// Buscando os dias da semana em que uma determinada espécie estará em exibição:
 const getSpeciesAvailability = (speciesName) => {
   const { availability } = species.find((dataSpecies) => dataSpecies.name === speciesName);
   return availability;
 };
 
+// Buscando o horário de funcionamento do zoológico em um determinado dia da semana:
 const getOfficeHour = (weekday) => {
   const { open, close } = hours[weekday];
   return open
@@ -15,6 +26,7 @@ const getOfficeHour = (weekday) => {
     : 'CLOSED';
 };
 
+// Buscando as espécies em exibição em um determinado dia da semana:
 const getExhibition = (weekday) => {
   const exhibition = species.filter(({ availability }) => availability.includes(weekday));
   return exhibition.length > 0
@@ -22,12 +34,14 @@ const getExhibition = (weekday) => {
     : 'The zoo will be closed!';
 };
 
+// Formatando as informações de um dia da semana com horário de funcionamento e exibições:
 const getDailySchedule = (weekday) => {
   const officeHour = getOfficeHour(weekday);
   const exhibition = getExhibition(weekday);
   return { [`${weekday}`]: { officeHour, exhibition } };
 };
 
+// Formatando as informações de todos os dias da semana com horários de funcionamento e exibições:
 const getWeeklySchedule = () => {
   let weeklySchedule = {};
 
@@ -40,6 +54,7 @@ const getWeeklySchedule = () => {
   return weeklySchedule;
 };
 
+// Função principal:
 const getSchedule = (scheduleTarget) => {
   const speciesNames = species.map(({ name }) => name);
 
