dd5e83fb1644ff7b4127a09076386023a4c5f9c6
felipe <brandaofelipebrandao@gmail.com>
2024-05-24 02:49:59 -0300

    Refatoração e descrição da função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,15 +1,30 @@
+/*
+  A função `getOldestFromFirstSpecies` busca o animal mais velho da primeira espécie pela qual uma pessoal colaboradora é responsável;
+  - Como parâmetro, a função recebe o `id` desse funcionário;
+  - Como retorno, a função devolve um array com nome, sexo e idade do animal mais velho da espécie.
+    - Exemplo: [ 'Shu', 'female', 19 ]
+*/
+
 const data = require('../data/zoo_data');
 
 const { employees, species } = data;
 
-const getOldestFromFirstSpecies = (id) => {
-  const speciesFound = employees.find((employee) => employee.id === id);
-  const animalId = speciesFound.responsibleFor[0];
-  const foundAnimal = species.find((animal) => animal.id === animalId);
-  const agesArray = [];
-  foundAnimal.residents.forEach((resident) => agesArray.push(resident.age));
-  const oldestAge = Math.max(...agesArray);
-  return Object.values(foundAnimal.residents.find((animal) => animal.age === oldestAge));
+const getOldestFromFirstSpecies = (employeeId) => {
+  // Encontrando a pessoa colaboradora e a primeira espécie pela qual ela é responsável:
+  const employee = employees.find(({ id }) => id === employeeId);
+  const speciesId = employee.responsibleFor[0];
+
+  // Encontrando a lista de animais dessa espécie:
+  const { residents } = species.find(({ id }) => id === speciesId);
+
+  // Mapeando a idade de todos os animais dessa lista:
+  const ages = residents.map(({ age }) => age);
+
+  // Buscando as informações completas sobre o animal mais velho:
+  const oldest = residents.find(({ age }) => age === Math.max(...ages));
+
+  // Retornando apenas um array com os valores de nome, sexo e idade do animal mais velho:
+  return Object.values(oldest);
 };
 
 module.exports = getOldestFromFirstSpecies;
