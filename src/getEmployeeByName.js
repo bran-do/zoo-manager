7aabad1dcee6703eeae1b991e76190e1e01ae636
felipe <brandaofelipebrandao@gmail.com>
2024-05-30 02:59:50 -0300

    Utils: getEmployeeByName + refatoração de tal

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -7,11 +7,9 @@ const data = require('../data/zoo_data');
 
 const { employees } = data;
 
-const getEmployeeByName = (name) => {
-  if (!name) { return {}; }
-
-  return employees.find(({ firstName, lastName }) =>
-    firstName === name || lastName === name);
-};
+const getEmployeeByName = (name) => (name
+  ? employees.find(({ firstName, lastName }) =>
+    firstName === name || lastName === name)
+  : {});
 
 module.exports = getEmployeeByName;
